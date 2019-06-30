a24ac109016d008e0385c0b5d88bc37bba3bc0b7
Noa <noasouccar@icloud.com>
2019-06-29 22:27:00 -0400

    Initial Commit

---
+++ CCBF/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CCBF
+//
+//  Created by Noa on 6/29/19.
+//  Copyright © 2019 Noa Souccar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CCBFTests/CCBFTests.swift
@@ -0,0 +1,34 @@
+//
+//  CCBFTests.swift
+//  CCBFTests
+//
+//  Created by Noa on 6/29/19.
+//  Copyright © 2019 Noa Souccar. All rights reserved.
+//
+
+import XCTest
+@testable import CCBF
+
+class CCBFTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
